f98ec518bdb8c1067bf221b36791be404bd17ff7
Sergio <se.diaz60@gmail.com>
2023-04-21 23:08:40 -0500

    [Update]: add url in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  urlGames : {
+    memory: 'https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=30'
+  }
 };
